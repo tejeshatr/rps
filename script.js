3cc585fe401773ad357274ac38dbcd81d4144777
tejeshatr <teammarvel0@gmail.com>
2018-08-05 16:29:47 +0530

    all done

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 var options = document.querySelectorAll('.option');
+var reset = document.querySelector('.reset');
+
+var player = 0, computer = 0;
+
+function rst(e)
+{
+	var p = document.querySelectorAll('.opt');
+	var q = document.querySelectorAll('.points');
+	for(let i = 0; i < 2; i++){
+		p[i].textContent = '';
+	}
+	q[0].textContent = 'You: 0';
+	q[1].textContent = 'Computer: 0';
+
+	p = document.querySelector('.end');
+	p.textContent='';
+
+}
 
 function play(e)
 {
@@ -10,23 +28,30 @@ function play(e)
 		if(p == options[i].textContent)
 			x = i;
 
-	display(p, options[c].textContent);
+	display(p, options[c].textContent, '.player', '.computer', 
+		'Your option: ', 'computer: ');
 
-	if(x == (c+1)%3)
+	if(x == (c+1)%3){
+		player++;
 		con.textContent = 'You win!';
+	}
 	else if(c == x)
 		con.textContent = 'Game Draw!';
-	else
+	else{
+		computer++;
 		con.textContent = 'You loose!';
+	}
+	display(player, computer, '.play', '.comp', 
+		'You: ', 'Computer: ');
 
 }
 
-function display(p,c)
+function display(p,c,s1,s2,m1,m2)
 {
-	let pl = document.querySelector('.player');
-	let co = document.querySelector('.computer');
-	pl.textContent = 'Your option: '+p;
-	co.textContent = "Computer: "+c;
+	let pl = document.querySelector(s1);
+	let co = document.querySelector(s2);
+	pl.textContent = m1+p;
+	co.textContent = m2+c;
 }
 
 function complay()
@@ -37,3 +62,5 @@ function complay()
 
 for(let i = 0; i < options.length; i++)
 	options[i].addEventListener('click', play);
+
+reset.addEventListener('click', rst);
